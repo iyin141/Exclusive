e9b04fcbb3e1b6d6e0edc67fa775abfb526b87e5
iyin141 <66314242+iyin141@users.noreply.github.com>
2025-03-18 22:53:08 +0100

    Eight commit

--- Videos/react/e-commerce/frontend/src/pages/Nav/Routes.tsx
+++ Videos/react/e-commerce/frontend/src/pages/Nav/Routes.tsx
@@ -1,12 +1,13 @@
-
+import { useNavigate } from "react-router-dom"
 
 const Routes = () => {
+  const navigate = useNavigate();
   return (
       <div>
           <ul className="flex gap-x-10 max-sm:gap-x-5  max-sm:pb-4    pt-1 text-center max-sm:justify-center max-sm:items-center">
-              <li className="border-b-1">Home</li>
-              <li>Contact</li>
-              <li>About</li>
+              <li className="border-b-1"><button onClick={() => navigate("/")}>Home</button></li>
+              <li><button onClick={() => navigate("/About")}>Contact</button></li>
+              <li><button onClick={() => navigate("/About")}>About</button></li>
               <li>Sign up</li>
           </ul>
     </div>
